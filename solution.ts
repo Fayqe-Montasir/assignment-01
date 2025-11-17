2a818109e2d7b4fbd75d6be5c3f5cc6fb05a8b8c
Fayqe-Montasir <swe.montasir@gmail.com>
2025-11-18 01:55:48 +0600

    problem-02 done

--- solution.ts
+++ solution.ts
@@ -1,16 +1,31 @@
 function formatValue(value : string | number | boolean ) : string | number | boolean {
 
     if(typeof value === "string"){
-        return value.toUpperCase()
+        return value.toUpperCase();
     } 
     else if(typeof value === "number"){
-        return (10* value)
+        return (10 * value);
     }
      else {
-        return !value
+        return !value;
     }
 }
+
 console.log(formatValue('hello'));
 console.log(formatValue(5));
 console.log(formatValue(true));
 
+
+function getLength(value : string | any[] ) : number { 
+    if(typeof value === "string"){
+    return value.length;
+    } 
+    else if (Array.isArray(value)){
+        return value.length;
+    }else{
+        return 0;
+    }
+}
+
+console.log(getLength('typescript'));
+console.log(getLength([10, 20, 30, 40]));
